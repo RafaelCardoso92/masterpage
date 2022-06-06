91b99683a2631d982e463f885a08140ec9495ac4
rafaelcardoso <rafael@goyuno.com>
2022-06-06 23:10:21 +0100

    removal of folder

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "741461de3b0d5052"; }
+/******/ 		__webpack_require__.h = function() { return "1f74c69408b10cd9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
